d9cb5491b2e7582c4561c95aa3f74d8ed44a668b
0xkzam <0xkzhunter@gmail.com>
2024-02-27 04:46:25 +0530

    d04 content

---
+++ d04/homework4/variables/variables2.rs
@@ -0,0 +1,12 @@
+// variables2.rs
+// Make me compile! Execute the command `zustlings hint variables2` if you want a hint :)
+
+
+fn main() {
+    let x:i32 = 0;
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
